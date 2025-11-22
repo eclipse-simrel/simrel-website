2261d5e0c5ce4003ee8e1c80e72c852c9e8ce26b
Ed Merks <ed.merks@gmail.com>
2025-11-22 10:09:03 +0100

    Refactor the markdown rendering to host related CBI project content
    - Several CBI projects are key to producing SimRel's results and will
    benefit from better and more accessible document.

--- project.js
+++ project.js
@@ -3,19 +3,34 @@ var eclipse_org_common = { "settings": { "cookies_class": { "name": "eclipse_set
 window.onscroll = function() {
 	const footer = document.querySelector("#footer>div>a");
 	footer.style.display = document.documentElement.scrollTop > 100 ? 'inline' : 'none';
+	updateTocSize();
 };
+window.onresize = updateTocSize;
 
 const scriptBase = new URL(".", document.currentScript.src).href
 const markdownBase = `${scriptBase}markdown/?file=`;
-const selfHostedMarkdownBase = `${scriptBase}markdown/?f=`;
-const newsBase = `${scriptBase}news/news.html?file=`;
+
+const file = getFileParameter();
+const dotGitHub = /(?<root>wiki|profile)\/(?<path>.*)/.exec(file);
+const parts = /(?<org>eclipse-(packaging|cbi))\/(?<repo>[^/]+)\/(?<branch>[^/]+)\/(?<path>.*)/.exec(file);
+const org = parts != null ? parts.groups.org : 'eclipse-simrel';
+const repo = parts != null ? parts.groups.repo : dotGitHub == null ? 'simrel-website' : '.github';
+const branch = parts != null ? parts.groups.branch : 'main';
+const path = parts != null ? parts.groups.path : dotGitHub == null ? file : `${dotGitHub.groups.root}/${dotGitHub.groups.path}`;
+const selfHosted = repo == 'simrel-website';
+const repoName = dotGitHub == null ? 'SimRel' : 'SimRel Wiki';
+
 const isLocalHost = window.location.hostname == 'localhost';
+const logicalBaseURL = new URL(`https://api.github.com/repos/${org}/${repo}/contents/${path}`);
+const apiURL = `${logicalBaseURL}?ref=${branch}`;
+const defaultURL = selfHosted ? `${scriptBase}${path.replace(/eclipse-simrel\/simrel-website\/main\//, '')}` : apiURL;
+const localURL = new URL(file, window.location);
 
 let meta = toElements(`
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" href="images/simrel-logo.svg"/>
+<link rel="shortcut icon" href="${(typeof favicon === 'undefined') ? 'images/simrel-logo.svg' : favicon}"/>
 `);
 
 let defaultHeader = toElements(`
@@ -65,7 +80,7 @@ let projectAside = `
 <a id="current-release-cycle-participants" href="">&nbsp;&nbsp;&nbsp;&nbsp;&bullet;&nbsp;Participants</a>
 <a href="${scriptBase}?file=wiki/SimRel/Overview.md">Overview</a>
 <a href="${scriptBase}?file=wiki/SimRel/Simultaneous_Release_Requirements.md">Requirements</a>
-<a href="${scriptBase}?file=report/report.md">Contributors</a>
+<a href="${scriptBase}?file=report/report.md">Contributor Report</a>
 <a href="${scriptBase}?file=wiki/SimRel/Simultaneous_Release_Cycle_FAQ.md">FAQ</a>
 `;
 
@@ -75,21 +90,35 @@ let githubAside = `
 <a href="https://github.com/eclipse-simrel/.github">Wiki</a>
 `;
 
+let cbiAsideProjects = `
+<a href="${scriptBase}cbi/?file=eclipse-cbi/p2repo-sbom/main/docs/index.md">p2 SBOM</a>
+<a href="${scriptBase}cbi/?file=eclipse-cbi/p2repo-aggregator/main/docs/user-guide.md">p2 Aggregator</a>
+<a href="${scriptBase}cbi/?file=eclipse-cbi/p2repo-analyzers/main/README.md">p2 Analyzers</a>
+`;
+
+let cbiAside = `
+<a class="separator" href="/simrel/cbi/"><i class='fa fa-cube'></i> CBI</a>
+${cbiAsideProjects}
+`;
+
 let defaultAside = toElements(`
 ${projectAside}
-${githubAside}
+${repo == '.github' && path == 'profile/README.md' ? githubAside : ''}
+${repo == '.github' && path == 'profile/README.md' ? cbiAside : ''}
 `);
 
-let tableOfContentsAside = '';
-
-function redirect(href) {
-	const location = href != null ?
-		`${href}${window.location.search}${window.location.hash}` :
-		new URL(`${selfHostedMarkdownBase}${window.location.pathname.replace(/^\/eclipse\//, '').replace(/\/$/, '/index.html').replace(/\.html$/, '.md')}`);
-	const body = document.querySelector('body')
-	replaceChildren(body, "body", ...toElements(`<div style="display: none">If you are not redirected automatically, 	follow this <a href='${location}'>link</a>.</div>`));
-	window.location = location;
-}
+let tableOfContentsAside = `
+<div id="toc" class="col-md-6">
+	<aside>
+		<ul class="ul-left-nav">
+			<div  class="sideitem">
+				<h2>Table of Contents</h2>
+				<div id="toc-target">
+				</div>
+			</div>
+		</ul>
+	</aside>
+</div>`;
 
 function generate() {
 	try {
@@ -109,17 +138,30 @@ function generate() {
 
 		const generatedBody = generateBody();
 		document.body.replaceChildren(...generatedBody);
+		generateBreadcrumbDetails(path.replace(/\/$/, ''));
+
+		if (!selfHosted && isLocalHost) {
+			const localURL = new URL(window.location);
+			localURL.hash = '';
+			localURL.search = '';
+			localURL.pathname = `${org}/${repo}/${branch}/${path}`;
+			fetch(localURL, { method: 'HEAD', cache: "no-store" }).then(response => {
+				if (response.status == 200 && response.headers.get('Server') == 'org.eclipse.oomph.internal.util.HTTPServer') {
+					defaultHandler(localURL);
+				} else {
+					defaultHandler(defaultURL);
+				}
+			});
+		} else {
+			defaultHandler(defaultURL);
+		}
 	} catch (exception) {
 		document.body.prepend(...toElements(`<span>Failed to generate content: <span><b style="color: FireBrick">${exception.message}</b><br/>`));
 		console.log(exception);
 	}
 }
 
-function generateDefaults(element, id) {
-	if (id != null && getQueryParameter(id) != null) {
-		return;
-	}
-
+function generateDefaults(element) {
 	const parts = [];
 	if (!hasElement('header')) {
 		parts.push(generateDefaultHeader(document.createElement('div')));
@@ -339,6 +381,10 @@ function generateDefaultNav(element) {
 	return prependChildren(element, 'nav', ...defaultNav);
 }
 
+let navImage = `${scriptBase}images/simrel-logo-with-title.svg`;
+
+let navLink = `${scriptBase}`;
+
 function generateNav() {
 	const elements = document.body.querySelectorAll('#nav>a');
 	if (elements.length == 0) {
@@ -366,8 +412,8 @@ function generateNav() {
 	return `
 <div class="header_nav">
 	<div class="col-xs-24 col-md-10 vcenter">
-		<a href="${scriptBase}">
-			<img id="header-nav-img" src="${scriptBase}images/simrel-logo-with-title.svg" alt="The Main Index Page" width="50%" xheight="auto" class="img-responsive header_nav_logo"/>
+		<a href="${navLink}">
+			<img id="header-nav-img" src="${navImage}" alt="The Main Index Page" width="50%" xheight="auto" class="img-responsive header_nav_logo"/>
 		</a>
 	</div><!-- NO SPACES
  --><div class="col-xs-24 col-md-14 vcenter">
@@ -417,6 +463,319 @@ function generateAside() {
 </div>
 `;
 }
+
+function getFileParameter() {
+	const search = new URLSearchParams(window.location.search);
+	return search.get('file') ?? ((typeof defaultMarkdown === 'undefined') ? 'profile/README.md' : defaultMarkdown);
+}
+
+function getMarkdownSearch(path) {
+	const dotGitHubParts = /eclipse-simrel\/.github\/main\/(?<path>.*)/.exec(path);
+	if (dotGitHubParts != null) {
+		return `?file=${dotGitHubParts.groups.path}`;
+	}
+	return `?file=${path}`;
+}
+
+function getTargetElement() {
+	return document.getElementById('markdown-target');
+}
+
+function fixHash(hash) {
+	return hash.toLowerCase();
+}
+
+function toSiteURL(url) {
+	if (url.hostname == 'api.github.com' && url.pathname.startsWith('/repos/eclipse-simrel/simrel-website/contents')) {
+		const result = new URL(window.location);
+		result.pathname = url.pathname.replace(/\/repos\/eclipse-simrel\/simrel-website\/contents/, '/simrel')
+		result.hash = url.hash;
+		result.search = url.search;
+		return result;
+	} else {
+		return null;
+	}
+}
+
+function generateBreadcrumbDetails(path) {
+	const breadcrumb = document.querySelector(".breadcrumb");
+	if (org == "eclipse-packaging") {
+		document.title = `SimRel | Eclipse Packaging`;
+		breadcrumb.append(...toElements(`<li>Eclipse Packaging</li>`));
+		const headerNavImg = document.getElementById('header-nav-img');
+		if (headerNavImg != null) {
+			headerNavImg.src = 'https://raw.githubusercontent.com/eclipse-packaging/.github/refs/heads/main/assets/artwork/svg/EPP-Color-Vertical.svg';
+		}
+	} else if (path.startsWith('report/report.md')) {
+		document.title = `SimRel | Contributors`;
+		breadcrumb.append(...toElements(`<li>Contributors</li>`));
+	} else if (path.startsWith('wiki/Simultaneous_Release.md')) {
+		document.title = `SimRel | Schedule`;
+		breadcrumb.append(...toElements(`<li>Schedule</li>`));
+	} else {
+		const matchRelease = /^wiki\/SimRel\/(?<name>[0-9-]+).md$/.exec(path);
+		if (matchRelease != null) {
+			const name = matchRelease.groups.name;
+			breadcrumb.append(...toElements(`<li><a href="${scriptBase}?file=wiki/Simultaneous_Release.md">Schedule</a></li> <li>${name}</li>`));
+			document.title = `SimRel | ${name}`;
+			generateParticipants(name);
+		} else {
+			const matchBasic = /^wiki\/SimRel\/(?<qualifier>Simultaneous_Release_Cycle_|Simultaneous_Release_|Contributing_to_Simrel_Aggregation_Build|Release_|)(?<name>.*).md$/.exec(path);
+			if (matchBasic != null) {
+				const name = niceName(matchBasic.groups.name);
+				const fixedName = name == '' ? 'Contibution' : name;
+				breadcrumb.append(...toElements(`<li>${fixedName}</li>`));
+				document.title = `SimRel | ${fixedName}`;
+			} else {
+				const participantMatch = /^wiki\/SimRel\/(?<name>.*)_participants.json$/.exec(path);
+				if (participantMatch != null) {
+					const name = participantMatch.groups.name;
+					document.title = `SimRel | ${name} Participants`;
+					breadcrumb.append(...toElements(`<li><a href="${scriptBase}?file=wiki/Simultaneous_Release.md">Schedule</a></li> <li><a href="${scriptBase}?file=wiki/SimRel/${name}.md">${name}</a></li> <li>Participants</li>`));
+				}
+			}
+		}
+	}
+}
+
+function generateParticipants(name) {
+	handleDocument('eclipse-simrel', '.github', 'main', `wiki/SimRel/${name}_participants.json`, content => {
+		if (!content.includes('"status":')) {
+			const element = document.getElementById('common-information');
+			if (element != null) {
+				const li = element.nextElementSibling.firstElementChild
+				li.parentNode.insertBefore(...toElements(`<li><a href="${scriptBase}?file=wiki/SimRel/${name}_participants.json">${name} partipating projects.</a></li>`), li);
+			}
+		}
+	});
+}
+
+function generateFileList(files) {
+	const fileElements = files.map(file => {
+		const fileURL = new URL(file.url);
+		const fileName = fileURL.pathname.endsWith('/profile') || fileURL.pathname.endsWith('/') || isLocalHost && file['type'] == 'dir' ?
+			/(?<filename>[^./][^/]+)\/?$/.exec(fileURL.pathname) :
+			/(?<filename>[^/]+)\.(md|json)$/.exec(fileURL.pathname);
+		if (fileName == null) {
+			return '';
+		}
+		const branch = fileURL.searchParams.get('ref');
+		const parts = /\/repos\/(?<org>[^/]+)\/(?<repo>[^/]+)\/contents\/(?<path>.*)/.exec(fileURL.pathname);
+		const url = new URL(window.location);
+		url.hash = '';
+		url.search = getMarkdownSearch(`${parts.groups.org}/${parts.groups.repo}/${branch}/${parts.groups.path}`.replace('//', '/'));
+		const label = niceName(fileName.groups.filename);
+		return `<div><a href="${url}">${label}<a/></div>\n`;
+	});
+
+	const folder = path.replace(/\/$/, '');
+	const heading = `<h2>Contents of ${org}/${repo}${folder.length == 0 ? '' : ` - ${folder}`}</h2>\n`;
+
+	getTargetElement().innerHTML = heading + fileElements.join('');
+}
+
+function generateReleaseParticipants(logicalBaseURL, response) {
+	const json = JSON.parse(response);
+	const release = Object.keys(json);
+	const projects = json[release].projects;
+	const projectList = Object.entries(projects).map(([key, value]) => {
+		const project_name = value.project_name;
+		const new_and_noteworthy_url = value.new_and_noteworthy_url;
+		const news = new_and_noteworthy_url == null ? '' : ` [**&#128240;**](${new_and_noteworthy_url})`;
+		return `1. [${project_name}](https://projects.eclipse.org/projects/${key})${news}`;
+	}).join('\n');
+	generateMarkdown(logicalBaseURL, `
+# [${release}](${release}.md)
+
+${projectList}
+
+# [${release} JSON](${release}_participants.json)
+
+<details>
+<summary>${release}_participants.json</summary>
+
+${"```"}
+${response}
+${"```"}
+</details>
+`)
+	const projectReferences = document.getElementById('markdown-target').querySelectorAll('a[href^="https://projects.eclipse.org/projects/"]');
+	for (const project of projectReferences) {
+		const projectId = project.href.toString().replace(/.*\//, '');
+		handleDocumentURL(new URL(`https://projects.eclipse.org/api/projects/${projectId.replace('.', '_')}`), content => {
+			const json = JSON.parse(content);
+			const projectJSON = json[0];
+			let projectLogo = 'https://projects.eclipse.org/modules/custom/eclipsefdn/eclipsefdn_projects/images/logos/default.png';
+			if (projectJSON != null) {
+				const logo = projectJSON.logo;
+				if (logo != null) {
+					projectLogo = logo;
+				}
+			}
+			project.parentElement.insertBefore(...toElements(`<span><img class="zoom" src="${projectLogo}"/>&nbsp;</span>`), project);
+		});
+	}
+}
+
+function generateMarkdown(logicalBaseURL, response) {
+	if (response instanceof Array) {
+		generateFileList(response);
+	} else if (response.startsWith('{')) {
+		if (logicalBaseURL.toString().endsWith("_participants.json")) {
+			generateReleaseParticipants(logicalBaseURL, response);
+		} else {
+			generateMarkdown(logicalBaseURL, `
+${"```"}
+${response}
+${"```"}
+`);
+		}
+	} else {
+		const text = response;
+		const editLink = logicalBaseURL.toString().includes('report.md') ? '' : `<a id="edit-markdown-link" href=""><span class="orange">\u270E Improve this page</span></a>\n`;
+		marked.use(markedGfmHeadingId.gfmHeadingId());
+		marked.use({
+			hooks: {
+				postprocess(html) {
+					return `${html}`;
+				}
+			}
+		});
+		getTargetElement().innerHTML = editLink + marked.parse(text);
+
+		const headings = markedGfmHeadingId.getHeadingList();
+		const headingText = `
+<ul id="table-of-contents">
+${headings.map(({ id, raw, level }) => `<li class="tl${level}"><a href="#${id}">${raw}</a></li>`).join(' ')}
+</ul>
+`;
+		console.log("Foo");
+		document.getElementById('toc-target').replaceChildren(...toElements(headingText));
+
+		const logo = getTargetElement().querySelector('img[src$="SimRel-Color.svg"]');
+		if (logo != null) {
+			const next = logo.nextElementSibling
+			logo.remove();
+			if (next != null) {
+				next.remove();
+			}
+		}
+
+		const imgs = getTargetElement().querySelectorAll("img[src]");
+		for (const img of imgs) {
+			const src = img.getAttribute('src');
+			if (src == null) {
+				continue;
+			}
+
+			if (!src.startsWith('http')) {
+				const logicalSrc = new URL(src, logicalBaseURL);
+				const siteURL = toSiteURL(logicalSrc);
+				if (siteURL != null) {
+					img.src = siteURL;
+				} else {
+					img.src = new URL(`https://raw.githubusercontent.com/${org}/${repo}/${branch}/${path}/../${src}`);
+				}
+			}
+		}
+
+		const as = getTargetElement().querySelectorAll("a[href]");
+		for (const a of as) {
+			const href = a.getAttribute('href');
+			if (href == null || href == '') {
+				continue;
+			}
+
+			if (href.startsWith('#')) {
+				a.setAttribute('href', fixHash(href));
+				continue;
+			}
+
+			const logicalHref = new URL(href, logicalBaseURL);
+			if (logicalHref.pathname.endsWith('report.svg')) {
+				a.href = 'report/report.svg';
+				continue;
+			} else if (!logicalHref.pathname.endsWith('.md')) {
+				if (!href.startsWith('http')) {
+					a.href = new URL(`https://github.com/${org}/${repo}/blob/${branch}/${path}/../${href}`);
+				}
+				continue;
+			}
+
+			const url = new URL(window.location);
+			url.hash = fixHash(logicalHref.hash);
+			if (logicalHref.hostname == 'api.github.com') {
+				const parts = /\/repos\/(?<org>[^/]+)\/(?<repo>[^/]+)\/contents\/(?<path>.*)/.exec(logicalHref.pathname);
+				if (parts != null) {
+					if (parts.groups.org == 'eclipse-simrel' || parts.groups.org == 'eclipse-packaging' || parts.groups.org == 'eclipse-cbi') {
+						url.search = getMarkdownSearch(`${parts.groups.org}/${parts.groups.repo}/${branch}/${parts.groups.path}`);
+						a.href = url;
+					}
+				}
+			} else if (logicalHref.hostname == 'github.com') {
+				const parts = /(?<org>[^/]+)\/(?<repo>[^/]+)\/blob\/(?<branch>[^/]+)\/(?<path>.*)/.exec(logicalHref.pathname);
+				if (parts != null) {
+					if (parts.groups.org == 'eclipse-simrel' || parts.groups.org == 'eclipse-packaging' || parts.groups.org == 'eclipse-cbi') {
+						url.search = getMarkdownSearch(`${parts.groups.org}/${parts.groups.repo}/${parts.groups.branch}/${parts.groups.path}`);
+						a.href = url;
+					}
+				}
+			}
+		}
+
+		const editMarkdownLink = document.getElementById('edit-markdown-link');
+		if (editMarkdownLink != null) {
+			editMarkdownLink.href = `https://github.com/${org}/${repo}/blob/${branch}/${path}`;
+		}
+
+		// Ensure that we nagivate to the target.
+		if (document.location.hash.includes('#')) {
+			document.location.hash = document.location.hash;
+		}
+
+		updateTocSize();
+	}
+}
+
+function defaultHandler(url) {
+	fetch(url).then(response => {
+		return response.text();
+	}).then(text => {
+		if (text.startsWith('<') && !url.toString().endsWith('.md')) {
+			if (text.startsWith('<img') || text.match(/<ul><li><a href="[^"]+"> Parent Directory<\/a><\/li>/)) {
+				const links = [...text.matchAll(/href="([^./][^"]+?(\.md|\/))"/g).map(match => {
+					return { url: `https://api.github.com/repos/${org}/${repo}/contents/${path}/${match[1]}?ref=${branch}` };
+				})];
+				generateFileList(links);
+			} else if (url != apiURL) {
+				getTargetElement().innerHTML = `Cannot produce directory listing ${url} redirecting to ${apiURL}.`;
+				defaultHandler(apiURL);
+			} else {
+				getTargetElement().innerHTML = `Cannot produce directory listing ${url}.`;
+			}
+		} else if (text.startsWith('{') || text.startsWith('[')) {
+			if (url.toString().endsWith('.json')) {
+				generateMarkdown(logicalBaseURL, text);
+			} else {
+				const json = JSON.parse(text);
+				generateMarkdown(logicalBaseURL, json instanceof Array ? json : blobToText(json.content));
+			}
+		} else {
+			generateMarkdown(logicalBaseURL, text);
+		}
+	});
+}
+
+function updateTocSize() {
+	const toc = document.getElementById('toc');
+	const tocInner = document.getElementById('table-of-contents');
+	if (toc != null && tocInner != null) {
+		const height = window.document.documentElement.clientHeight;
+		tocInner.style.maxHeight = `${height - tocInner.getBoundingClientRect().top - 20}px`;
+		toc.style.maxHeight = `${height - toc.getBoundingClientRect().top - 20}px`;
+	}
+}
+
 
 function getCurrentReleaseCycle() {
 	handleDocument('eclipse-simrel', 'simrel.build', 'main', 'simrel.aggr', content => {
